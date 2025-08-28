7e09bd12ffa0f966f6c464c4aef9630a0ed3d826
saikishore0319 <saikishor20172017@gmail.com>
2025-08-28 12:42:36 +0000

    adding product pahhe

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const redirect = ()=>{
     navigate('/collection')
-    setShowSearch(! showSearch);
+    setShowSearch(true);
 
   }
 
