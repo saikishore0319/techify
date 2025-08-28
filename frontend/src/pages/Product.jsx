87ff7ac9a574a65f7bdd53470d36b9a1bb872bad
saikishore0319 <saikishor20172017@gmail.com>
2025-08-28 17:23:50 +0000

    added related products section

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,12 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
   const { productId } = useParams();
-  const { products } = useContext(ShopContext);
+  const { products, currency } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('')
+  const [size, setSize] = useState('')
 
   const fetchProductData = async () => {
     products.map((item) => {
@@ -29,11 +32,67 @@ const Product = () => {
         {/* product images */}
         <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row '>
           <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
-            <img src={image} alt='' />
+            {
+              productData.image.map((item, index) => (
+                <img onClick={() => setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
+              ))
+            }
+          </div>
+          <div className='w-full sm:w-[80%] '>
+            <img className='w-full  h-auto' src={image} alt="" />
+          </div>
+        </div>
+
+        {/* product information  */}
+        <div className='flex-1'>
+          <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+          <div className='flex items-center gap-1 mt-2 '>
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+            <p className='pl-2'>(122)</p>
+          </div>
+          <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+          <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+
+          <div className='flex flex-col gap-4 my-8'>
+            <p>Select Size</p>
+            <div className='flex gap-2 '>
+              {
+                productData.sizes.map((item, index) => (
+                  <button onClick={() => setSize(item)} className={`border py-2 px-4 bg-gray-100 cursor-pointer hover:border-orange-400 ${item === size ? 'border-orange-500' : ''} `} key={index} >{item}</button>
+                ))
+              }
+            </div>
+          </div>
+
+          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700 cursor-pointer'>ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5 ' />
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1 '>
+            <p>100% Original Product</p>
+            <p>Cash on delivery is available on this product</p>
+            <p>Easy return and exchange policy within 7 days</p>
           </div>
         </div>
       </div>
 
+      {/* description and review section */}
+      <div className='mt-20'>
+          <div className='flex '>
+              <b className='border px-5 py-3 text-sm '>Description</b>
+              <p className='border px-5 py-3 text-sm '>Reviews(122)</p>
+          </div>
+          <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum sint harum reiciendis odio aspernatur, maiores minima laboriosam neque assumenda ut </p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quibusdam iste explicabo labore optio. Doloribus sed sunt quisquam ut vero nostrum </p>
+          </div>
+
+      </div>
+
+      {/* display related products */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
     </div>
   ) : <div className='opacity-0'></div>
 }
