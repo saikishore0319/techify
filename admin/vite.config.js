d20ca554655e5e176ac7105891ed2cbf48ffc1f6
saikishore0319 <saikishor20172017@gmail.com>
2025-08-31 17:26:14 +0000

    added admin project, changed frontend ports

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,13 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    port: 5173,
-    // proxy:{
-    //   '/api': "http://localhost:3000"
-    // },
-  },
   plugins: [react(),tailwindcss()],
+  server:{port: 5174}
 })
